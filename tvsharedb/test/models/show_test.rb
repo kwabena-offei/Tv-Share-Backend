ad16908257f0a0e3e7812cc9963df1f617e6b142
Dylan Montgomery <mail@citizensinspace.com>
2020-07-02 19:09:38 -0400

    Ensure show.tmsId is unique

--- tvsharedb/test/models/show_test.rb
+++ tvsharedb/test/models/show_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ShowTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "tmsId must be unique" do
+    tms_id = '123'
+    show_1 = Show.create(tmsId: tms_id)
+    show_2 = Show.new(tmsId: tms_id)
+
+    assert show_1.valid?
+    refute show_2.valid?
+  end
 end
