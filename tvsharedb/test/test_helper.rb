1065d62436aedd0151b06825e986bdd7d18b2d6a
Dylan Montgomery <mail@citizensinspace.com>
2020-07-06 13:20:32 -0400

    Setup VCR, Wemock, and DotENV for testing

--- tvsharedb/test/test_helper.rb
+++ tvsharedb/test/test_helper.rb
@@ -1,6 +1,16 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'webmock'
+require 'vcr'
+
+# Records external HTTP requests for testing purposes
+VCR.configure do |config|
+  config.cassette_library_dir = "test/vcr_cassettes"
+  config.hook_into :webmock
+  # Prevents TMS_API_KEY from being saved in the recorded files
+  config.filter_sensitive_data('<TMS_API_KEY>') { ENV['TMS_API_KEY'] }
+end
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
