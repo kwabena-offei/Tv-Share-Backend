4e655395fa668b8d58421cc4ba6d248cf054ce36
Dylan Montgomery <mail@citizensinspace.com>
2020-07-28 11:22:36 -0400

    Allow users to login via username or email

--- tvsharedb/test/controllers/shows/originals_controller_test.rb
+++ tvsharedb/test/controllers/shows/originals_controller_test.rb
@@ -10,18 +10,16 @@ class Shows::OriginalsControllerTest < ActionDispatch::IntegrationTest
   test "returns all original shows on index" do
     get shows_originals_url, as: :json
     assert_response :success
-    json = JSON.parse(body)
 
-    assert_equal 2, json.count
-    assert_equal [@hulu_show.id, @netflix_show.id].sort, json.map { |show| show['id'] }.sort
+    assert_equal 2, json_response.count
+    assert_equal [@hulu_show.id, @netflix_show.id].sort, json_response.map { |show| show['id'] }.sort
   end
 
   test "returns network specific originals on show" do
     get network_shows_originals_path(network: :hulu), as: :json
     assert_response :success
-    json = JSON.parse(body)
 
-    assert_equal 1, json.count
-    assert_equal @hulu_show.title, json[0]['title']
+    assert_equal 1, json_response.count
+    assert_equal @hulu_show.title, json_response[0]['title']
   end
 end
