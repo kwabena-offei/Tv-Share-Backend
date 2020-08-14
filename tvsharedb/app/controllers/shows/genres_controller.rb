f66a96b30ab2b962e7411ed62caea207596e5f7f
Dylan Montgomery <mail@citizensinspace.com>
2020-08-14 14:45:52 -0400

    Genre pagination

--- tvsharedb/app/controllers/shows/genres_controller.rb
+++ tvsharedb/app/controllers/shows/genres_controller.rb
@@ -1,24 +1,23 @@
 class Shows::GenresController < ActionController::Base
-  PAGE_SIZE = 10000
-  caches_action :index, expires_in: 1.hour
+  PAGE_SIZE = 25
+  caches_action :index, expires_in: 1.hour, if: -> { Rails.env.production? }
 
   # all genres each with the first PAGE_SIZE shows
   def index
-    @shows = GenreMap.to_h.reduce({}) do |memo, (title, subgenres)|
+    @genre_shows = GenreMap.to_h.reduce({}) do |memo, (title, subgenres)|
       memo[title] = Show.with_tms_id.non_episode
         .select(:id, :title, :preferred_image_uri, :tmsId, :seriesId, :rootId)
         .by_genres(subgenres)
         .order(:title)
-        .limit(PAGE_SIZE)
+        .page(1)
+        .per(PAGE_SIZE)
       memo
     end
-
-    render json: @shows
   end
 
   def show
     @genre = params[:genre]
-    sub_genres = GenreMap.to_h[params[:genre]]
-    @shows = data = Show.with_tms_id.non_episode.by_genres(sub_genres).order(:title).page(params[:page]).limit(PAGE_SIZE)
+    sub_genres = GenreMap.to_h[@genre]
+    @shows = data = Show.with_tms_id.non_episode.by_genres(sub_genres).order(:title).page(params[:page]).per(PAGE_SIZE)
   end
 end
