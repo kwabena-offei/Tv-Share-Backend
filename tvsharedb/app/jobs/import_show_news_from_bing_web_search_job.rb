cdfa97bfe0f7b37e8e1e39d192c6517ab55c3909
Dylan Montgomery <mail@citizensinspace.com>
2020-11-02 17:57:57 -0500

    Tweak scraper

--- tvsharedb/app/jobs/import_show_news_from_bing_web_search_job.rb
+++ tvsharedb/app/jobs/import_show_news_from_bing_web_search_job.rb
@@ -2,7 +2,6 @@ require 'whatlanguage'
 
 class ImportShowNewsFromBingWebSearchJob < ApplicationJob
   RESULT_COUNT = 50
-  PAGES_TO_SCRAPE = 3
   attr_accessor :show
   queue_as :low_priority
 
@@ -16,31 +15,29 @@ class ImportShowNewsFromBingWebSearchJob < ApplicationJob
   def import
     existing_show_stories_urls = Set.new(show.stories.pluck(:url))
     @language_parser = WhatLanguage.new(:all)
+    get_search_terms(show).each do |search_term|
+      retrieve_search_results(search_term).each do |url|
+        next if existing_show_stories_urls.include?(url)
+        existing_show_stories_urls.add(url)
 
-    urls_to_scrape = get_search_terms(show).flat_map do |search_term|
-      retrieve_search_results(search_term)
-    end.uniq
+        metadata = get_story_metadata(url)
+        next unless metadata['og:type'] == 'article'
+        next if metadata['og:locale'].present? && !metadata['og:locale']&.starts_with?('en')
+        next unless metadata['og:description'].present? && is_english?(metadata['og:description'])
+        next if metadata['og:title'].blank? || metadata['og:title']&.downcase&.include?('wiki')
+        next if metadata['og:site_name'].present? && metadata['og:site_name']&.downcase&.include?('wiki') || metadata['og:site_name']&.downcase&.include?('pastebin')
 
-    urls_to_scrape.each do |url|
-      next if existing_show_stories_urls.include?(url)
-
-      metadata = get_story_metadata(url)
-      next unless metadata['og:type'] == 'article'
-      next if metadata['og:locale'].present? && !metadata['og:locale']&.starts_with?('en')
-      next unless metadata['og:description'].present? && is_english?(metadata['og:description'])
-      next if metadata['og:title'].blank? || metadata['og:title']&.downcase&.include?('wiki')
-      next if metadata['og:site_name'].present? && metadata['og:site_name']&.downcase&.include?('wiki') || metadata['og:site_name']&.downcase&.include?('pastebin')
-
-      import_story({
-        url: url,
-        title: metadata['og:title'],
-        source: metadata['og:site_name'],
-        description: metadata['og:description'],
-        image_url: metadata['og:image'],
-        published_at: metadata['article:published_time'] || show.origAirDate
+        import_story({
+          url: url,
+          title: metadata['og:title'],
+          source: metadata['og:site_name'],
+          description: metadata['og:description'],
+          image_url: metadata['og:image'],
+          published_at: metadata['article:published_time'] || show.origAirDate
         })
       end
     end
+  end
 
   def import_story(story_data)
     story = Story.find_or_initialize_by(url: story_data[:url])
@@ -77,16 +74,16 @@ class ImportShowNewsFromBingWebSearchJob < ApplicationJob
 
   def retrieve_search_results(query)
     url = "https://api.cognitive.microsoft.com/bing/v7.0/search?q=#{URI.escape(query)}&count=#{RESULT_COUNT}"
+
     response = HTTParty.get(url, {
       headers: {
         'Ocp-Apim-Subscription-Key' => ENV['BING_API_KEY'],
         'User-Agent' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
-      },
-      timeout: 5
+      }
     })
 
     json = JSON.parse(response.body)
-    json.dig('webPages', 'value')&.map { |result| result['url'] } || []
+    json.dig('webPages','value')&.map { |result| result['url'] } || []
   rescue  => e
     puts e
     []
@@ -101,9 +98,9 @@ class ImportShowNewsFromBingWebSearchJob < ApplicationJob
     false
   end
 
-
   def get_story_metadata(url)
-    Timeout::timeout(5) do # BUG. Never do this.
+    response = ''
+    Timeout::timeout(5) do
       response = HTTParty.get(url,
         headers: {
           'User-Agent' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
@@ -125,7 +122,8 @@ class ImportShowNewsFromBingWebSearchJob < ApplicationJob
     properties
 
   # If the article couldn't be retrieved or parsed, move on to the next URL.
-  rescue
+  rescue => e
+    puts e
     {}
   end
 end
