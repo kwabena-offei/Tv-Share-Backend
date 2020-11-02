eab5b22d6f31019f78c948f1b8989cc6c318cede
Dylan Montgomery <mail@citizensinspace.com>
2020-11-02 14:03:51 -0500

    Implement algolia searching for comments

--- tvsharedb/app/models/comment.rb
+++ tvsharedb/app/models/comment.rb
@@ -1,7 +1,27 @@
 class Comment < ApplicationRecord
+  include AlgoliaSearch
+
+  algoliasearch enqueue: true do
+    attributes [:show_title, :short_text, :preview_image, :show_id]
+    searchableAttributes [:text, 'unordered(short_text)']
+    customRanking ['desc(likes_count)', 'desc(sub_comments_count)', 'desc(shares_count)']
+  end
+
   belongs_to :user
   belongs_to :show, counter_cache: true
   has_many :likes, dependent: :destroy
   has_many :sub_comments, dependent: :destroy
   has_many :shares, as: :shareable
+
+  def show_title
+    show&.title
+  end
+
+  def short_text
+    text&.truncate(500)
+  end
+
+  def preview_image
+    images&.first
+  end
 end
