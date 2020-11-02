8939375c86bb313c0886bbc43d3cbed5765d75cd
Dylan Montgomery <mail@citizensinspace.com>
2020-11-02 15:31:42 -0500

    Add a timeout to bing search

--- tvsharedb/app/jobs/import_show_news_from_bing_web_search_job.rb
+++ tvsharedb/app/jobs/import_show_news_from_bing_web_search_job.rb
@@ -82,7 +82,8 @@ class ImportShowNewsFromBingWebSearchJob < ApplicationJob
       headers: {
         'Ocp-Apim-Subscription-Key' => ENV['BING_API_KEY'],
         'User-Agent' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
-      }
+      },
+      timeout: 5
     })
 
     json = JSON.parse(response.body)
