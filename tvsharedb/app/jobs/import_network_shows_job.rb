64f1479d94374d0047a5962a329df1e224cdb393
Dylan Montgomery <mail@citizensinspace.com>
2020-11-04 22:37:01 -0500

    Optimize network show import

--- tvsharedb/app/jobs/import_network_shows_job.rb
+++ tvsharedb/app/jobs/import_network_shows_job.rb
@@ -1,32 +1,58 @@
+require 'set'
+
 class ImportNetworkShowsJob < ApplicationJob
+  attr_accessor :network, :series_ids, :non_series_tms_ids
   queue_as :default
 
   def perform(network)
-    import_network_shows(network)
+    @network = network
+    @series_ids = Set.new
+    @non_series_tms_ids = Set.new
+    import_network_shows
   end
 
-  def import_network_shows(network)
-    get_station_airings(network).each do |airing|
-      show = Show.includes(:networks).find_or_import_by_tms_id(airing['program']['tmsId'])
-      next unless show.present?
+  def import_network_shows
+    get_station_program_data
+    import_series
+    import_non_series
+  end
 
-      if show.seriesId.present?
-        Show.where(seriesId: show.seriesId).find_each do |_show|
-          _show.networks << network unless _show.networks.include?(network)
-        end
-      else
-        show.networks << network unless show.networks.include?(network)
+  def import_series
+    @series_ids.each do |series_id|
+      Show.includes(:networks).where(seriesId: series_id).find_each do |_show|
+        _show.networks << network unless _show.networks.include?(network)
       end
-
     end
   end
 
-  def get_station_airings(network)
-    response = HTTParty.get api_url(network.station_id)
-    JSON.parse(response.body)
+  def import_non_series
+    @non_series_tms_ids.each do |tms_id|
+      show = Show.includes(:networks).find_or_import_by_tms_id(tms_id)
+      next if show.blank?
+      show.networks << network unless show.networks.include?(network)
+    end
   end
 
   private
+
+  def get_station_data
+    response = HTTParty.get api_url(@network.station_id)
+    JSON.parse(response.body)
+  end
+
+  def get_station_program_data
+    get_station_data.reduce({}) do |memo, airing|
+      program = airing['program']
+      series_id = program['seriesId']
+      tms_id = program['tmsId']
+
+      if series_id
+        @series_ids.add(series_id)
+      else
+        @non_series_tms_ids.add(tms_id)
+      end
+    end
+  end
 
   def api_url(station_id)
     "http://data.tmsapi.com/v1.1/stations/#{station_id}/airings?startDateTime=#{7.days.ago.iso8601}&endDateTime=#{14.days.from_now.iso8601}&api_key=#{ENV['TMS_API_KEY']}"
