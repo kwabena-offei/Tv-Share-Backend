4a5fdd64402368ade085120be49f0f52c031af9c
Dylan Montgomery <mail@citizensinspace.com>
2020-11-08 22:07:21 -0500

    Update ImportLiveGuide

--- tvsharedb/app/jobs/import_live_guide_job.rb
+++ tvsharedb/app/jobs/import_live_guide_job.rb
@@ -11,60 +11,43 @@ class ImportLiveGuideJob < ApplicationJob
     api_response = HTTParty.get(api_url)
 
     api_response.each do |result|
-      network_name = result['affiliateCallSign'] || result['callSign']
+      station_id = result['stationId']
       result['airings'].each do |airing|
         program = airing['program']
-        import_show_or_parent(program, network_name)
+        import_show_or_parent(program, station_id)
       end
     end
   end
 
-  def import_show_or_parent(program, network_name)
-    if program['tmsId'].match(/^(SH|MV).*/)
-      # We have a root show or movie, import as-is
-      import_show(program, network_name)
-    else
+  def import_show_or_parent(program, station_id)
+    import_show(program, station_id)
+
+    if program['tmsId'].start_with?('EP')
       # We have an episode, we need to request parent series data
       show = Show.includes(:networks).find_by(seriesId: program['seriesId'])
       if show.present?
         # the show is already in our db, so ensure it is associated with this network
-        assign_network(show, network_name)
+        assign_network(show, station_id)
       else
         # the show is not in our db, do a full import
         data = get_parent_show_data(program)
-        import_show(data, network_name)
+        import_show(data, station_id)
       end
     end
   end
 
-  def import_show(program, network_name)
-    show = Show.includes(:networks).find_or_initialize_by(tmsId: program['tmsId'])
-    show.update({
-      rootId: program['rootId'],
-      seriesId: program['seriesId'],
-      subType: program['subType'],
-      title: program['title'],
-      releaseYear: program['releaseYear'],
-      releaseDate: program['releaseDate'],
-      origAirDate: program['origAirDate'],
-      titleLang: program['titleLang'],
-      descriptionLang: program['descriptionLang'],
-      entityType: program['entityType'],
-      genres: program['genres'],
-      longDescription: program['longDescription'],
-      shortDescription: program['shortDescription'],
-      runTime: program['runTime'],
-      preferred_image_uri: program.dig('preferredImage', 'uri')
-    })
-
-    assign_network(show, network_name)
+  def import_show(program, station_id)
+    show = Show.find_or_import_by_tms_id(program['tmsId'])
+    assign_network(show, station_id) if show.present?
   end
 
-  def assign_network(show, network_name)
-    unless show.networks.pluck(:name).include?(network_name)
-      network = Network.find_or_initialize_by(name: network_name)
-      show.networks << network
-      show.save
+  def assign_network(show, station_id)
+    unless show.networks.pluck(:station_id).include?(station_id)
+      network = Network.find_by(station_id: station_id)
+      if network.present?
+        show.networks << network
+        show.save
+      end
     end
   end
 
