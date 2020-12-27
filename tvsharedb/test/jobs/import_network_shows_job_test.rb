ba9b55d4af1cda30587c487af0896ce513627c1f
Dylan Montgomery <mail@citizensinspace.com>
2020-12-27 12:49:29 -0500

    Reimport full show data in ImportNetworkShowJob

--- tvsharedb/test/jobs/import_network_shows_job_test.rb
+++ tvsharedb/test/jobs/import_network_shows_job_test.rb
@@ -4,14 +4,15 @@ class ImportNetworkShowsJobTest < ActiveJob::TestCase
   setup do
     network = Network.create(station_id: 16689)
     VCR.use_cassette("network_shows") do
-      Timecop.freeze('2020-11-03') do
+      Timecop.freeze('2020-12-17') do
         ImportNetworkShowsJob.perform_now(network)
       end
     end
   end
 
   test "imports network shows" do
-    show = Show.find_by(tmsId: 'EP032582570351')
+
+    show = Show.find_by(tmsId: 'EP000191907092')
     assert_equal 1, show.networks.count
     assert_equal '16689', show.networks.first.station_id
   end
