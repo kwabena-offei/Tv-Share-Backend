e9dcb3ae26fd7c0f752919f290b057bfc785b779
Dylan Montgomery <mail@citizensinspace.com>
2021-03-14 19:53:22 -0400

    Cache genres behind the scenes

---
+++ tvsharedb/test/controllers/shows/genres_controller_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+class Shows::GenresControllerTest < ActionDispatch::IntegrationTest
+
+  test "GET index" do
+    get shows_genres_url
+    assert_response :success
+
+    assert_equal ["Action & Hero Stuff",
+      "Adventure Stuff",
+      "Cartoon & Animated Stuff",
+      "Cooking, Dressing & Decorating",
+      "Cops & Lawyers Stuff",
+      "Cowboy Stuff",
+      "Cultured Stuff",
+      "Earth Stuff",
+      "Entertainment",
+      "Funny Stuff",
+      "Game Shows",
+      "Guilty Pleasure",
+      "Love Stuff",
+      "People Talking",
+      "Potentially Dramatic",
+      "Scary Stuff",
+      "Sci-Fi Stuff",
+      "Smart Stuff",
+      "Soap Opera Stuff",
+      "Sports Stuff"
+    ], response.parsed_body.keys
+
+    assert_equal ['results', 'pagination'], response.parsed_body["Action & Hero Stuff"].keys
+    assert_equal ['total_count', 'next_page'], response.parsed_body["Action & Hero Stuff"]['pagination'].keys
+  end
+
+  test "GET show" do
+    genre = 'Action & Hero Stuff'
+    get genre_shows_genres_url(genre: genre)
+    assert_response :success
+
+    assert_equal genre, response.parsed_body['genre']
+    assert_equal ["genre", "results", "pagination"], response.parsed_body.keys
+    assert_equal ["total_count", "current_page", "total_pages", "page_size", "next_page"], response.parsed_body['pagination'].keys
+  end
+end
