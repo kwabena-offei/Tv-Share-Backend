9b8f384ebb92464df7f4b2694b59520d869e7b58
Dylan <742111+citizens@users.noreply.github.com>
2021-06-03 18:43:24 -0400

    Increase login session from 1 day to 90 days (#112)

--- tvsharedb/app/controllers/application_controller.rb
+++ tvsharedb/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
 
-  def encode(payload, exp = 24.hours.from_now)
+  def encode(payload, exp = 90.days.from_now)
     payload[:exp] = exp.to_i
     JWT.encode(payload, SECRET_KEY)
   end
