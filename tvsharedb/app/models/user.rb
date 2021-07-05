98c207f42884c044730720b75e3b4887e5873e96
Dylan Montgomery <mail@citizensinspace.com>
2021-07-05 15:00:18 -0500

    Username and email case insensitive validations

--- tvsharedb/app/models/user.rb
+++ tvsharedb/app/models/user.rb
@@ -4,11 +4,10 @@ class User < ApplicationRecord
 
   validate :password_complexity
   validates :username, :email, presence: true
-  validates :username, :email, uniqueness: true
+  validates :username, :email, uniqueness: { case_sensitive: false }
   validates :facebook_id, :email, uniqueness: true, allow_nil: true
   validates :google_id, :email, uniqueness: true, allow_nil: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-
 
   has_many :comments, dependent: :destroy
   has_many :sub_comments, dependent: :destroy
