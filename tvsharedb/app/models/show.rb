0f4a2fc06e56c34e93521d5f76ac270f8a7405c5
Dylan <742111+citizens@users.noreply.github.com>
2021-08-13 15:15:37 -0500

    HBO Max network and importer (#123)

--- tvsharedb/app/models/show.rb
+++ tvsharedb/app/models/show.rb
@@ -1,5 +1,11 @@
 class Show < ApplicationRecord
-  enum original_streaming_network: { netflix: 0, hulu: 1, prime: 2 }
+  enum original_streaming_network: {
+    netflix: 0,
+    hulu: 1,
+    prime: 2,
+    hbo_max: 3
+  }
+
   acts_as_votable cacheable_strategy: :update_columns
   include AlgoliaSearch
 
