c8b8bec075f41ec13dc184e8c1aaa949e9999e59
Dylan <742111+citizens@users.noreply.github.com>
2021-12-07 19:57:05 -0600

    Enable category ordering (#146)

--- tvsharedb/app/controllers/categories_controller.rb
+++ tvsharedb/app/controllers/categories_controller.rb
@@ -6,7 +6,7 @@ class CategoriesController < ActionController::Base
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.active.includes(shows: :networks)
+    @categories = Category.active.order(:position).includes(shows: :networks)
   end
 
   # GET /categories/1
