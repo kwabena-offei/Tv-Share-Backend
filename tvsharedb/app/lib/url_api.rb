62c04885c34f4687c6be7ba53a7e7a915ddb4f69
kwabena-offei <kabboa.offei@gmail.com>
2025-05-31 21:28:03 -0400

    Use ENV var for TMS API key

--- tvsharedb/app/lib/url_api.rb
+++ tvsharedb/app/lib/url_api.rb
@@ -1,9 +1,8 @@
 class UrlApi
   include HTTParty
   def self.fetch start_date, end_date, networks
-    api_url = "https://data.tmsapi.com/v1.1/lineups/USA-HULU501-DEFAULT/grid?startDateTime=#{start_date}&endDateTime=#{end_date}&stationId=#{networks.map{|network| network[:stationId]}}&imageAspectTV=4x3&imageSize=Lg&api_key=v5nfdpmz66hp2nd5t9gefcrc"
+    api_url = "https://data.tmsapi.com/v1.1/lineups/USA-HULU501-DEFAULT/grid?startDateTime=#{start_date}&endDateTime=#{end_date}&stationId=#{networks.map{|network| network[:stationId]}}&imageAspectTV=4x3&imageSize=Lg&api_key=#{ENV['TMS_API_KEY']}"
     resp = HTTParty.get(api_url)
-    byebug
-    data = resp.body
+    resp.body
   end
 end
