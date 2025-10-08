11bd5e7169ad9e53c23f81a69feec596e0e1555a
Heath Christman <heath.christman@gmail.com>
2025-10-07 20:53:45 -0500

    Prioritize selected season for episode import

--- tvsharedb/app/controllers/shows/episodes_controller.rb
+++ tvsharedb/app/controllers/shows/episodes_controller.rb
@@ -1,91 +1,79 @@
 class Shows::EpisodesController < ApplicationController
-  
   def index
-    Rails.logger.info "EpisodesController#index called with id=#{params[:id]}, season=#{params[:season]}"
+   # Rails.logger.info "EpisodesController#index called with id=#{params[:id]}, season=#{params[:season]}"
     
-    @show = find_or_import_show
-    if @show.blank?
-      Rails.logger.error "EpisodesController: Could not find or import show #{params[:id]}"
-      return head(:not_found)
+    @series = find_parent_series
+    if @series.blank?
+      Rails.logger.error "EpisodesController: Could not find or import parent series for #{params[:id]}"
+      return render json: { episodes: [], importing: true }
     end
     
-    Rails.logger.info "EpisodesController: Found show #{@show.tmsId}, seriesId=#{@show.seriesId}, entityType=#{@show.entityType}"
-    
-    # Get the series ID - works for both show and episode tmsIds
-    series_id = @show.seriesId
-    
-    if series_id.blank?
-      Rails.logger.error "EpisodesController: Show has no seriesId"
-      return head(:not_found)
-    end
-    
-    # If specific season requested, fetch just that season (with on-demand import)
-    if params[:season].present?
-      @episodes = fetch_season_episodes(series_id, params[:season])
-      Rails.logger.info "EpisodesController: Returning #{@episodes.count} episodes for season #{params[:season]}"
-      render json: @episodes
+   # Rails.logger.info "EpisodesController: Found series #{@series.tmsId}, seriesId=#{@series.seriesId}"
+  
+    episodes_in_db_count = Show.where(seriesId: @series.seriesId, entityType: 'Episode').count
+    total_episodes_from_metadata = @series.totalEpisodes.to_i
+    count_mismatch = total_episodes_from_metadata > 0 && episodes_in_db_count < total_episodes_from_metadata
+
+    is_importing = if count_mismatch
+      latest_episode = Show.where(seriesId: @series.seriesId, entityType: 'Episode').order(created_at: :desc).first
+      time_since_last_import = latest_episode ? Time.current - latest_episode.created_at : nil
+      
+      time_since_last_import && time_since_last_import < 15.seconds
     else
-      # Return all episodes grouped by season
-      # Only require seasonNum and episodeNum
-      @episodes_by_season = Show.where(seriesId: series_id)
+      false
+    end
+
+    if params[:season].present?
+      season_num = params[:season]
+      
+      # Run a synchronous import for the requested season.
+      if @series.seriesId.present?
+      #  Rails.logger.info "EpisodesController: Prioritizing season #{season_num} import."
+        import_season_episodes(@series.seriesId, season_num)
+      end
+
+      # After the priority import, fetch the definitive list of episodes for this season.
+      episodes = Show.where(seriesId: @series.seriesId, seasonNum: season_num)
+                     .where.not(episodeNum: nil)
+                     .order(:episodeNum)
+      
+      final_importing_flag = false
+
+     # Rails.logger.info "EpisodesController: Returning #{episodes.count} episodes for season #{season_num}."
+      render json: { episodes: episodes, importing: final_importing_flag }
+    else
+      @episodes_by_season = Show.where(seriesId: @series.seriesId)
                                 .where.not(seasonNum: nil, episodeNum: nil)
                                 .order(:seasonNum, :episodeNum)
                                 .group_by(&:seasonNum)
                                 
-      Rails.logger.info "EpisodesController: Returning episodes for #{@episodes_by_season.keys.count} seasons"
-      render json: @episodes_by_season
+     # Rails.logger.info "EpisodesController: Returning episodes for #{@episodes_by_season.keys.count} seasons. Importing: #{is_importing}"
+      render json: { episodes: @episodes_by_season.values.flatten, importing: is_importing }
     end
   end
 
   private
 
-  def find_or_import_show
-    # Note: params[:id] could be either a tmsId or a seriesId
-    # Try tmsId first
-    show = Show.find_by(tmsId: params[:id])
-    
-    if show.blank?
-      Rails.logger.info "EpisodesController: Show #{params[:id]} not found by tmsId, trying seriesId..."
-      # Try to find by seriesId
-      show = Show.find_by(seriesId: params[:id], entityType: 'Show')
-      
-      if show.blank?
-        Rails.logger.info "EpisodesController: Not found by seriesId either, importing..."
-        # Try importing - could be either tmsId or seriesId
-        if params[:id].start_with?('SH', 'MV', 'EP')
-          # It's a tmsId
-          ImportShowJob.perform_now(tmsId: params[:id], import_episodes: false)
-          show = Show.find_by(tmsId: params[:id])
-        else
-          # It's a seriesId (numeric)
-          ImportShowJob.perform_now(seriesId: params[:id], import_episodes: true)
-          show = Show.find_by(seriesId: params[:id], entityType: 'Show')
-        end
-      end
-    else
-      Rails.logger.info "EpisodesController: Found show #{show.tmsId} by tmsId"
+  def find_parent_series
+    show_or_episode = Show.find_by(tmsId: params[:id])
+
+    series_id = if show_or_episode
+      show_or_episode.seriesId
+    elsif params[:id].match?(/^\d+$/)
+      params[:id]
+    end
+
+    return nil if series_id.blank?
+
+    parent_series = Show.find_by(seriesId: series_id, entityType: 'Show')
+
+    if parent_series.blank?
+     # Rails.logger.info "EpisodesController: Parent series #{series_id} not found, importing metadata now."
+      ImportShowJob.perform_now(seriesId: series_id, import_episodes: false)
+      parent_series = Show.find_by(seriesId: series_id, entityType: 'Show')
     end
     
-    show
-  end
-  
-  def fetch_season_episodes(series_id, season_num)
-    # Only require episodeNum (not episodeTitle) (handles soap operas, etc.)
-    episodes = Show.where(seriesId: series_id, seasonNum: season_num)
-                   .where.not(episodeNum: nil)
-                   .order(:episodeNum)
-    
-    # If no episodes found for this season, import them on-demand
-    if episodes.empty? && series_id.present?
-      Rails.logger.info "EpisodesController: No episodes found for season #{season_num}, importing on-demand..."
-      import_season_episodes(series_id, season_num)
-      episodes = Show.where(seriesId: series_id, seasonNum: season_num)
-                     .where.not(episodeNum: nil)
-                     .order(:episodeNum)
-      Rails.logger.info "EpisodesController: After import, found #{episodes.count} valid episodes"
-    end
-    
-    episodes
+    parent_series
   end
   
   def import_season_episodes(series_id, season_num)
@@ -97,20 +85,18 @@ class Shows::EpisodesController < ApplicationController
     Rails.logger.info "EpisodesController: Gracenote API response type: #{api_response.class}"
     
     if api_response && api_response.is_a?(Array)
-      # Response is directly an array of episodes
-      Rails.logger.info "EpisodesController: Importing #{api_response.length} episodes from array"
+     # Rails.logger.info "EpisodesController: Importing #{api_response.length} episodes from array"
       api_response.each do |episode_data|
         ImportShowJob.new.import_show(episode_data) if episode_data
       end
     elsif api_response && api_response['hits']
-      # Response has 'hits' wrapper
-      Rails.logger.info "EpisodesController: Importing #{api_response['hits'].length} episodes from hits"
+     # Rails.logger.info "EpisodesController: Importing #{api_response['hits'].length} episodes from hits"
       api_response['hits'].each do |episode_data|
         program = episode_data['program'] || episode_data
-        ImportShowJob.new.import_show(program) if program
+      #  ImportShowJob.new.import_show(program) if program
       end
     else
-      Rails.logger.error "EpisodesController: Unexpected API response format"
+      Rails.logger.error "EpisodesController: Unexpected API response format for series #{series_id}, season #{season_num}"
     end
   end
 end
