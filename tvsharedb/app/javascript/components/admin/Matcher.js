f8eb363ef5c2742970035ff73eac9b6fd4ae2b63
Heath Christman <heath.christman@gmail.com>
2025-10-08 05:34:19 -0500

    Admin Matching Panel: Original Networks now shows visual success on clicking match

--- tvsharedb/app/javascript/components/admin/Matcher.js
+++ tvsharedb/app/javascript/components/admin/Matcher.js
@@ -74,8 +74,8 @@ class Matcher extends React.Component {
       body: JSON.stringify(data)
     }).then(response => response.json())
     .then(data => {
+      this.setState({ selectedShow: data });
       this.getData();
-      this.getPossibleMatches(selectedShow, selectedId, selectedTitle, tmsId);
     });
   }
 
@@ -96,6 +96,7 @@ class Matcher extends React.Component {
     }).then(response => response.json())
     .then(data => {
       this.getData();
+      this.setState({ selectedShow: data })
     });
   }
 
